ced4cb688e848f479b4e3eb5bca008b63e467207
ncluff003 <ncluff003@zohomail.com>
2025-07-03 14:54:55 -0600

    Implemented the full content, style, and script for the home index page.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,16 @@
+const currentLocation = document.querySelector("#current-location");
 const currentYear = document.querySelector("#currentyear");
-currentYear.innerText = new Date.now().year;
+const lastModified = document.querySelector("#lastModified");
+
+currentLocation.innerText = "Sandy, Utah";
+currentYear.innerText = new Date().getFullYear();
+lastModified.innerText = `Last Updated: ${new Date(document.lastModified).toLocaleString("en", {
+  weekday: "long",
+  month: "long",
+  day: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZoneName: "long",
+})}`;
