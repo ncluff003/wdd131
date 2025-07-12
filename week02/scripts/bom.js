1ec32f7d7651b434388a4de28bf93bae6e2e39cd
ncluff003 <ncluff003@zohomail.com>
2025-07-12 15:00:24 -0600

    Implemented Book of Mormon Application functionality.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,20 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-
-li.textContent = input.value;
-deleteButton.innerText = "❌";
-deleteButton.ariaLabel = `Remove ${li.textContent}`;
-
-list.append(li);
+button.addEventListener("click", (event) => {
+  if (input.value.trim() !== "") {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    li.textContent = input.value;
+    deleteButton.innerText = "❌";
+    deleteButton.ariaLabel = `Remove ${li.textContent}`;
+    deleteButton.addEventListener("click", (event) => {
+      list.removeChild(li);
+      input.focus();
+    });
+    li.append(deleteButton);
+    list.append(li);
+    input.value = "";
+    return input.focus();
+  }
+});
