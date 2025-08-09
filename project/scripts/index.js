611766256907481a8ac3425c3f790fcb9cc26036
ncluff003 <ncluff003@zohomail.com>
2025-08-09 15:32:48 -0600

    Implemented most of the landing page.

---
+++ project/scripts/index.js
@@ -0,0 +1,24 @@
+const navigation = document.querySelector("#navigation");
+const hamburgerMenu = document.querySelector("#menu");
+
+console.log(navigation, hamburgerMenu);
+
+hamburgerMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  navigation.classList.toggle("show");
+});
+
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.innerText = new Date().getFullYear();
+lastModified.innerText = `Last Updated: ${new Date(document.lastModified).toLocaleString("en", {
+  weekday: "long",
+  month: "long",
+  day: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZoneName: "long",
+})}`;
